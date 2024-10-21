9abbda9191f81e23587ab2919488d740490cc58a
hazemr2121 <ha.ahmed8996@gmail.com>
2024-10-21 10:31:53 +0300

    feat: add global styled css and fix Project name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/input";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check in</Button>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
