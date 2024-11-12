bd999c79f065626440467f27acda6c1ec5902537
hazemr2121 <ha.ahmed8996@gmail.com>
2024-11-12 07:02:45 +0200

    feat: Add booking table and Empty component for empty tables and sort cabin table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
@@ -11,13 +12,21 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
   const filterValue = searchParams.get("discount") || "all";
-
+  if (!cabins.length) return <Empty resource="Cabins" />;
   let filtertedCabins;
   if (filterValue === "all") filtertedCabins = cabins;
   if (filterValue === "no-discount")
     filtertedCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filtertedCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // Sort
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filtertedCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -31,7 +40,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filtertedCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
