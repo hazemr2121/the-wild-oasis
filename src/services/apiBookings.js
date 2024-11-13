3671620b66d2c05eb187894ce229360c43343288
hazemr2121 <ha.ahmed8996@gmail.com>
2024-11-13 09:59:08 +0200

    feat: prefeatch data

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,10 +1,11 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
-    .select("*, cabins(name), guests(fullName,email)");
+    .select("*, cabins(name), guests(fullName,email)", { count: "exact" });
 
   //filter
   if (filter) query = query.eq(filter.field, filter.value);
@@ -14,12 +15,19 @@ export async function getBookings({ filter, sortBy }) {
       ascending: sortBy.direction === "asc",
     });
 
-  const { data, error } = await query;
+  //Pagination
+  if (page) {
+    const from = (page - 1) * (PAGE_SIZE - 1);
+    const to = from + PAGE_SIZE - 1;
+    query = query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
-  return data;
+  return { data, count };
 }
 export async function getBooking(id) {
   const { data, error } = await supabase
