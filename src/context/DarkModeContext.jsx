747cc1e01991da0907f560ec522ad09aa73e1c09
hazemr2121 <ha.ahmed8996@gmail.com>
2024-11-18 07:47:53 +0200

    feat: dark-mode 🌑

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,38 @@
+import { createContext } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+import { useContext } from "react";
+import { useEffect } from "react";
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.add("light-mode");
+    } else {
+      document.documentElement.classList.remove("dark-mode");
+      document.documentElement.classList.add("light-mode");
+    }
+  }, [isDarkMode]);
+
+  function toggleDarkMode() {
+    setIsDarkMode((prev) => !prev);
+  }
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (!context) {
+    throw new Error("useDarkMode must be used within a DarkModeProvider");
+  }
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
