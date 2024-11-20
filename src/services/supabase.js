70346be586630be8960a3bf8f05383b1f7429c1f
hazemr2121 <ha.ahmed8996@gmail.com>
2024-11-20 21:22:43 +0200

    fix: supabase error

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-
 export const supabaseUrl = "https://smxhnrbajtkllhirdxyj.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNteGhucmJhanRrbGxoaXJkeHlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2NTUzNjAsImV4cCI6MjA0NTIzMTM2MH0.HeqeMUaji9hbPcML0iMVAoii6I2QWM67O6OThVQbBjU";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
